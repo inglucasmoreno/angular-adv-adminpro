8814edbb3e4bbd76bba3205e8a382031cd0d8048
Lucas Omar Moreno <ing.lucasmoreno@gmail.com>
2020-09-22 00:17:12 -0300

    Fin seccion 8

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -13,11 +13,11 @@ export class SidebarService {
         { titulo: 'Main', url: '/' },
         { titulo: 'ProgressBar', url: '/dashboard/progress' },
         { titulo: 'Graficas', url: '/dashboard/grafica1' },
+        { titulo: 'Promesas', url: '/dashboard/promesas' },
+        { titulo: 'Rxjs', url: '/dashboard/rxjs' },
       ]
     }
   ];
-
-
 
   constructor() { }
 }
